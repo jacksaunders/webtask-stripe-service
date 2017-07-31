95de1311af5a16e0ee07555a47c27dddc2dded89
Jack Saunders <jacksaundersdesign@gmail.com>
2017-07-31 20:08:36 +0100

    Fix stripe error

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var app = new (require("express"))();
+var stripe = new (require("stripe"))();
 var wt = require("webtask-tools");
 var jwt = require("express-jwt");
-var stripe = require("stripe")();
 
 app.use((req, res, next) => {
   jwt({
@@ -19,7 +19,6 @@ app.get("/", function(req, res) {
   res.end("Hello, world!");
 });
 
-sub_AyBm7U84aclSG6;
 app.get("/subscriptions/delete", function(req, res) {
   stripe.subscriptions.del(
     req.webtaskContext.data.subscriptionID,
