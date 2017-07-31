aa015ae4a279a859845cfbb9043d898955b94cab
Jack Saunders <jacksaundersdesign@gmail.com>
2017-07-31 19:57:49 +0100

    Add Stripe

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var app = new (require("express"))();
 var wt = require("webtask-tools");
 var jwt = require("express-jwt");
+var jwt = require("stripe")();
 
 app.use((req, res, next) => {
   jwt({
@@ -16,6 +17,22 @@ app.use((req, res, next) => {
 
 app.get("/", function(req, res) {
   res.end("Hello, world!");
+});
+
+sub_AyBm7U84aclSG6;
+app.get("/subscriptions/delete", function(req, res) {
+  stripe.subscriptions.del(
+    "sub_B74qyT5AUM9U8l",
+    { api_key: req.webtaskContext.secrets.STRIPE_SECRET },
+    function(err, confirmation) {
+      if (err) {
+      }
+
+      if (confirmation) {
+        res.send(confirmation);
+      }
+    }
+  );
 });
 
 module.exports = wt.fromExpress(app).auth0();
