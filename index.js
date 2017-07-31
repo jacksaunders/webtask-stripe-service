b24a19329bca03b0bbdbba98751e69a76f916914
Jack Saunders <jacksaundersdesign@gmail.com>
2017-07-31 19:09:14 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var app = new (require("express"))();
+var wt = require("webtask-tools");
+var jwt = require("express-jwt");
+
+app.use((req, res, next) => {
+  jwt({
+    secret:
+      "-----BEGIN CERTIFICATE-----\n" +
+      req.webtaskContext.secrets.AUTH0_PUBLIC_KEY.match(/.{1,64}/g).join("\n") +
+      "\n-----END CERTIFICATE-----\n",
+    algorithms: ["RS256"],
+    issuer: `https://${req.webtaskContext.secrets.AUTH0_DOMAIN}/`,
+    audience: req.webtaskContext.secrets.AUTH0_CLIENT_ID
+  })(req, res, next);
+});
+
+app.get("/", function(req, res) {
+  res.end("Hello, world!");
+});
+
+module.exports = wt.fromExpress(app).auth0();
